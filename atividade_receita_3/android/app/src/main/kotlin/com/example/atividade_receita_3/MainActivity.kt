7de2c5ff196605ec146f35477d43576a8cf24e32
italo <italogithub@gmail.com>
2023-04-12 23:11:10 -0300

    feat: add receita 3 e 4

---
+++ atividade_receita_3/android/app/src/main/kotlin/com/example/atividade_receita_3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_receita_3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ atividade_receita_4/android/app/src/main/kotlin/com/example/atividade_receita_4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_receita_4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
