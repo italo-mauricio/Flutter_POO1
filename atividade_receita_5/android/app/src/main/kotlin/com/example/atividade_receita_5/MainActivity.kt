bab3fb100a1c2eaebf97b79b1b40f35d035b6a75
italo <italogithub@gmail.com>
2023-04-20 23:38:54 -0300

    update

---
+++ atividade_receita_5/android/app/src/main/kotlin/com/example/atividade_receita_5/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_receita_5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ atividade_receita_6/android/app/src/main/kotlin/com/example/atividade_receita_6/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.atividade_receita_6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
