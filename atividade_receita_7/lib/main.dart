5741e7949ec0f6293a704695f2b33b0ba0c69311
italo <italogithub@gmail.com>
2023-05-07 13:37:00 -0300

    update

--- atividade_receita_7/lib/main.dart
+++ atividade_receita_7/lib/main.dart
@@ -1,115 +1,165 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+class DataService {
+  final ValueNotifier<List> tableStateNotifier = new ValueNotifier([]);
+  final ValueNotifier<List<String>> columnNamesNotifier = new ValueNotifier([]);
+  final ValueNotifier<List<String>> propertyNamesNotifier =
+      new ValueNotifier([]);
+
+  DataService() {
+    loadBeers();
+  }
+
+  void load(index) {
+    switch (index) {
+      case 1:
+        loadCoffees();
+        break;
+      case 2:
+        loadBeers();
+        break;
+      case 3:
+        loadNations();
+        break;
+    }
+  }
+
+  Future<void> loadBeers() async {
+    var beersUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/beer/random_beer',
+        queryParameters: {'size': '5'});
+
+    var jsonString = await http.read(beersUri);
+
+    var beersJson = jsonDecode(jsonString);
+
+    propertyNamesNotifier.value = ["name", "brand", "ibu"];
+    columnNamesNotifier.value = ["Name", "Brand", "IBU"];
+    tableStateNotifier.value = beersJson;
+  }
+
+  Future<void> loadCoffees() async {
+    var coffeesUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/coffee/random_coffee',
+        queryParameters: {'size': '5'});
+
+    var jsonString = await http.read(coffeesUri);
+
+    var coffeesJson = jsonDecode(jsonString);
+
+    propertyNamesNotifier.value = ["blend_name", "origin", "variety"];
+    columnNamesNotifier.value = ["Blend Name", "Origin", "Variety"];
+    tableStateNotifier.value = coffeesJson;
+  }
+
+  Future<void> loadNations() async {
+    var nationsUri = Uri(
+        scheme: 'https',
+        host: 'random-data-api.com',
+        path: 'api/nation/random_nation',
+        queryParameters: {'size': '5'});
+
+    var jsonString = await http.read(nationsUri);
+
+    var nationsJson = jsonDecode(jsonString);
+
+    columnNamesNotifier.value = ["Nationality", "Language", "Capital"];
+    propertyNamesNotifier.value = ["nationality", "language", "capital"];
+    tableStateNotifier.value = nationsJson;
+  }
+}
+
+final dataService = DataService();
+
 void main() {
-  runApp(const MyApp());
+  MyApp app = MyApp();
+
+  runApp(app);
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Dicas"),
+          ),
+          body: ValueListenableBuilder(
+              valueListenable: dataService.tableStateNotifier,
+              builder: (_, value, __) {
+                return DataTableWidget(jsonObjects: value);
+              }),
+          bottomNavigationBar: NewNavBar(itemSelectedCallback: (index) {
+            dataService.load(index + 1);
+          }),
+        ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class NewNavBar extends HookWidget {
+  final _itemSelectedCallback;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  NewNavBar({itemSelectedCallback})
+      : _itemSelectedCallback = itemSelectedCallback ?? (int) {}
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    var state = useState(1);
+
+    return BottomNavigationBar(
+        onTap: (index) {
+          state.value = index;
+
+          _itemSelectedCallback(index);
+        },
+        currentIndex: state.value,
+        items: const [
+          BottomNavigationBarItem(
+            label: "Coffes",
+            icon: Icon(Icons.coffee_outlined),
+          ),
+          BottomNavigationBarItem(
+              label: "Beers", icon: Icon(Icons.local_drink_outlined)),
+          BottomNavigationBarItem(
+              label: "Nations", icon: Icon(Icons.flag_outlined))
+        ]);
+  }
+}
+
+class DataTableWidget extends StatelessWidget {
+  final List jsonObjects;
+
+  DataTableWidget({required this.jsonObjects});
+
+  @override
+  Widget build(BuildContext context) {
+    final columnNames = dataService.columnNamesNotifier.value;
+    final propertyNames = dataService.propertyNamesNotifier.value;
+    return DataTable(
+        columns: columnNames
+            .map((name) => DataColumn(
+                label: Expanded(
+                    child: Text(name,
+                        style: TextStyle(fontStyle: FontStyle.italic)))))
+            .toList(),
+        rows: jsonObjects
+            .map((obj) => DataRow(
+                cells: propertyNames
+                    .map((propName) => DataCell(Text(obj[propName])))
+                    .toList()))
+            .toList());
   }
 }
